9802dd8fa63f1612ce83d441d4dc185a665b5836
Hari Vardhan Reddy Peddireddy <Harivardhanreddy.Peddireddy@blueyonder.com>
2024-05-14 20:27:12 +0530

    14-May-2024

--- DSA/Programs/pattern-problems.py
+++ DSA/Programs/pattern-problems.py
@@ -1,15 +1,36 @@
+#Pattern problems
 #1  Square Pattern
 # Outer Loop Represents the rows
 # Inner Loop Represents the Columns
+
+# Improve Though process
 
 for i in range(4):
   for j in range(4):
     print(i,end=" ")
   print()
 
-for i in range(4,8):
-  for j in range(1,5):
+#2
+'''
+* * * * *
+* * * *
+* * *
+* *
+*
+'''
+
+
+# Tricks
+# 1: How to Appproach
+# Every rows(outer loop) and columns(inner loop)
+'''
+no of lines == no of rows
+no of columns == no of elements added
+
+'''
+n =5
+for i in range(n):
+  for j in range(i+1):
     print(i,end=" ")
   print()
 
-#2
