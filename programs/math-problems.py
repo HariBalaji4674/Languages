7c374bab858e99cdfa0925bedafef71df3bb995b
Hari Vardhan Reddy Peddireddy <Harivardhanreddy.Peddireddy@blueyonder.com>
2024-05-20 21:15:55 +0530

    20-May-2024

--- programs/math-problems.py
+++ programs/math-problems.py
@@ -29,3 +29,30 @@ print(out)
 
 # Palindrome or Not
 
+number = int(input("Please enter your palindrome check number: "))
+
+temp = number
+out = 0
+while number >0:
+  rem = number % 10
+  out = out*10 + rem
+  number = number //10
+
+if out == temp:
+  print("This is Palindrome Number")
+else:
+  print("This is not palindrome number")
+
+# Armstrong Number:
+number = int(input("Please Enter Armstrong Number: "))
+out = 0
+temp = number
+while number > 0:
+  rem = number % 10
+  out = out + rem * rem * rem
+  number = number // 10
+
+if out == temp:
+  print(f"{out} is an armstrong number")
+else:
+  print(f"{out} is not an armstrong number")
